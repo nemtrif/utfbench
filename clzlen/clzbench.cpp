c770877cd0638a1601da5302e44bbdb2f19c4aef
Nemanja Trifunovic <ntrifunovic@hotmail.com>
2025-10-11 15:23:01 -0400

    Add cyrilic data seed

--- clzlen/clzbench.cpp
+++ clzlen/clzbench.cpp
@@ -10,15 +10,23 @@
 using namespace std;
 
 constexpr string_view ascii_text = "The quick brown fox jumps over the lazy dog";
+constexpr u8string_view cyrilic_html = u8"<i>Телефонско</i> то је <b>звоно</b> <em>мога</em> апарата.";
 
-void generate_test_data(vector<unsigned char>& data) {
-    constexpr int data_multiplier = 1'000'000;
-    data.resize(ascii_text.size() * data_multiplier);
-    for (int i = 0; i < data_multiplier; ++i) {
-        data.insert(data.end(), ascii_text.begin(), ascii_text.end());
+enum class SeedKind { ASCII, CYRILLIC };
+
+void generate_test_data(vector<unsigned char>& data, SeedKind kind) {
+    constexpr int data_multiplier = 10'000'000;
+    data.clear();
+    if (kind == SeedKind::ASCII) {
+        data.reserve(ascii_text.size() * data_multiplier);
+        for (int i = 0; i < data_multiplier; ++i)
+            data.insert(data.end(), ascii_text.begin(), ascii_text.end());
+    } else {
+        data.reserve(cyrilic_html.size() * data_multiplier);
+        for (int i = 0; i < data_multiplier; ++i)
+            data.insert(data.end(), cyrilic_html.begin(), cyrilic_html.end());
     }
 }
-
 
 int countlz_utf8_sequence_length(unsigned char lead_byte) {
     switch (std::countl_one(lead_byte)) {
@@ -31,10 +39,18 @@ int countlz_utf8_sequence_length(unsigned char lead_byte) {
 }
 
 
-int main() {
+int main(int argc, char** argv) {
     // generate data
     vector<unsigned char> data;
-    generate_test_data(data);
+    // choose seed from command line: default = ascii, pass "cyr" to use cyrilic_html
+    SeedKind kind = SeedKind::ASCII;
+    if (argc > 1) {
+        string arg = argv[1];
+        if (arg == "cyr") {
+            kind = SeedKind::CYRILLIC;
+        }
+    }
+    generate_test_data(data, kind);
 
     // CSV header
     cout << "time_ms,items,MB_per_s\n";
